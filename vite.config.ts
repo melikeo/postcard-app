33aaf4b03d3a17d2372f4a7e42cba258da59b86a
melikeo <98155324+melikeo@users.noreply.github.com>
2025-05-09 21:07:54 +0200

    base link fix

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-   base: "https://github.com/melikeo/postcard-app",
+   base: "https://github.com/melikeo/postcard-app.git",
 })
